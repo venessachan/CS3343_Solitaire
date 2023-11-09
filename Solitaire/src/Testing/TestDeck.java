521f6a9eac86d88b5dbdb2e15d530d31ea6e15a9
t-philippelam <t-philippelam@IT-HMYBYJ3.hk.shkp.com>
2023-11-09 17:51:21 +0800

    Test Deck

--- Solitaire/src/Testing/TestDeck.java
+++ Solitaire/src/Testing/TestDeck.java
@@ -10,18 +10,6 @@ import card.Deck;
 
 public class TestDeck {
 	
-	//Common method for testing
-	private static String printDeck(List<Card> cards) {
-		String result = "";
-		for(Card c: cards) 
-		{
-			c.flip();
-			result += c.toString() + " ";
-		}
-		result = result.trim();
-		return result;
-	}
-	
 	private static boolean deckEquality(List<Card> c1, List<Card> c2) {
 		if(c1.size() != c2.size())
 			return false;
@@ -33,19 +21,6 @@ public class TestDeck {
 			}
 		}
 		return true;
-	}
-	
-	@Test
-	public void testInitialCards() {
-		
-		String ans =  "♠A ♠2 ♠3 ♠4 ♠5 ♠6 ♠7 ♠8 ♠9 ♠10 ♠J ♠Q ♠K "
-					+ "♥A ♥2 ♥3 ♥4 ♥5 ♥6 ♥7 ♥8 ♥9 ♥10 ♥J ♥Q ♥K "
-					+ "♣A ♣2 ♣3 ♣4 ♣5 ♣6 ♣7 ♣8 ♣9 ♣10 ♣J ♣Q ♣K "
-					+ "♦A ♦2 ♦3 ♦4 ♦5 ♦6 ♦7 ♦8 ♦9 ♦10 ♦J ♦Q ♦K";
-		
-		Deck deck = new Deck();
-		String result= printDeck(deck.getCards());
-		assertEquals(ans, result);
 	}
 	
 	@Test
@@ -61,49 +36,6 @@ public class TestDeck {
 		Deck deck2 = new Deck();
 		boolean ans = true;
 		assertEquals(ans, deckEquality(deck1.getCards(), deck2.getCards()));
-	}
-	
-	
-	@Test
-	public void testShuffleCards1() {//same seed give same result every time seed(0)
-		
-		String ans =  "♥3 ♠A ♥A ♠6 ♥J ♠10 ♠J ♦3 ♦9 ♦4 ♣9 ♣Q ♥5 "
-					+ "♦J ♦5 ♣A ♠2 ♥7 ♣K ♥Q ♣2 ♣6 ♦10 ♣8 ♥9 ♥K "
-					+ "♠5 ♦Q ♣J ♥2 ♠Q ♦2 ♥10 ♠4 ♣4 ♠7 ♣5 ♦K ♠K "
-					+ "♦6 ♥4 ♠8 ♥6 ♣10 ♥8 ♠3 ♦8 ♦A ♣7 ♠9 ♣3 ♦7";
-		
-		Deck deck = new Deck();
-		deck.shuffle(0);
-		String result= printDeck(deck.getCards());
-		assertEquals(ans, result);
-	}
-	
-	@Test
-	public void testShuffleCards2() {//seed(100)
-		
-		String ans =  "♦A ♠10 ♣A ♣6 ♠Q ♦4 ♦9 ♥2 ♣2 ♦8 ♦Q ♦3 ♣3 "
-					+ "♥5 ♥8 ♠4 ♣4 ♣8 ♥10 ♦5 ♥J ♣J ♣5 ♥K ♥3 ♠2 "
-					+ "♥7 ♠K ♣9 ♥9 ♣K ♦6 ♠8 ♥6 ♦10 ♠J ♠A ♠7 ♦7 "
-					+ "♦J ♠5 ♣Q ♠9 ♣10 ♦2 ♠3 ♥Q ♠6 ♥A ♦K ♣7 ♥4";
-		
-		Deck deck = new Deck();
-		deck.shuffle(100);
-		String result= printDeck(deck.getCards());
-		assertEquals(ans, result);
-	}
-	
-	@Test
-	public void testShuffleCards3() {//different seed give different result
-		
-		String ans =  "♦A ♠10 ♣A ♣6 ♠Q ♦4 ♦9 ♥2 ♣2 ♦8 ♦Q ♦3 ♣3 "
-					+ "♥5 ♥8 ♠4 ♣4 ♣8 ♥10 ♦5 ♥J ♣J ♣5 ♥K ♥3 ♠2 "
-					+ "♥7 ♠K ♣9 ♥9 ♣K ♦6 ♠8 ♥6 ♦10 ♠J ♠A ♠7 ♦7 "
-					+ "♦J ♠5 ♣Q ♠9 ♣10 ♦2 ♠3 ♥Q ♠6 ♥A ♦K ♣7 ♥4";
-		
-		Deck deck = new Deck();
-		deck.shuffle(0);
-		String result= printDeck(deck.getCards());
-		assertEquals(false, ans.equals(result));
 	}
 	
 	@Test
